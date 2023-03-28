4387036420767cc17db66263ad6142ae923190d6
michelwijnja@gmail.com <michelwijnja@gmail.com>
2023-03-28 15:47:48 +0200

    clean build.

---
+++ build/readme.txt
@@ -0,0 +1,7 @@
+To compile & install manual:
+
+$ cmake ..
+$ make
+$ make install
+
+Then in set insmod priveleges ~/bin/./setroot
--- build/src/hal/components/opencascade/cpp_interface/CMakeFiles/myApp_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/myApp_autogen"
-  "myApp_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/myApp_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
